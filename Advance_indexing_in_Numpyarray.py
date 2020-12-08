ee433fbe758e9c90495b788d2ee7b2547aef13e1
Devanshu Kumar <48691331+devanshu464@users.noreply.github.com>
2020-12-08 20:11:02 +0530

    Add files via upload

---
+++ Advance_indexing_in_Numpyarray.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec  8 11:08:47 2020
+
+@author: HP
+"""
+
+import numpy as np
+'''l = [10,20,30,40,50,60]
+a = np.array(l)
+print(a)
+print()
+print(a[0])
+print()
+print(a[4])
+print()
+print(a[7])'''
+
+'''l = [[10,20,30],[40,50,60],[70,80,90]]
+a = np.array(l)
+print(a)
+print(a[0])
+print()
+print(a[-1])
+print(a[0][0])
+print()
+print(a[-3][-3])'''
+
+l = [[[10,20,30],[40,50,60]],[[70,80,90],[100,110,120]]]
+a = np.array(l)
+print(a)
+print()
+print(a[0])
+print()
+print(a[1])
+print()
+
+
